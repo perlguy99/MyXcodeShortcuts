4502e5a4f13b6501a0d30025cec67296291d8bdf
Brent Michalski <brent.michalski@gmail.com>
2024-04-18 17:51:49 -0500

    Refactored view

--- MyXcodeShortcuts/Views/CategorySelectionView.swift
+++ MyXcodeShortcuts/Views/CategorySelectionView.swift
@@ -10,106 +10,76 @@ import SwiftData
 
 // TODO: When the view is presented, place a check on the row that is currently selected.
 
-// TODO: There should be a TextField at the top that is blank with a placeholder
-
-// TODO: that says "New Category"
-
-// TODO: Then we should have a LIST of all categories and a COUNT of all shortcuts in that category
-
 // TODO: Upon tapping a row - OR - having text in the TextField and hitting the "back" button
-
-// TODO: the data should get updated in the database.
-
-// TODO: Called by EditShortcutView
-
-// TODO: It seems to make sense to just pass it in a Shortcut object, doesn't it?
-
-
 
 struct CategorySelectionView: View {
     @Environment(\.modelContext) var modelContext
     @Environment(\.dismiss) var dismiss
     
     @State var shortcut: Shortcut
-    
     @Query private var categories: [Category]
-    
     @State private var tempCategoryName: String = ""
     
-    private let placeholder1 = "Category Name"
-    private let placeholder2 = "Or, Enter New Category"
-    
     var body: some View {
-        
         NavigationView {
             Form {
-                TextField(categories.isEmpty ? placeholder1 : placeholder2, text: $tempCategoryName)
-                    .textFieldStyle(.roundedBorder)
-                    .padding(.bottom, 20)
-                
-                if categories.isEmpty {
-                    Text("No categories yet").italic()
-                } else {
-                    listCategories
-                }
+                categoryTextField
+                categoryListOrMessage
+                Spacer()
             }
             .navigationTitle("Categories")
-            .onSubmit {
-                insertNewCategoryAndStoreInShortcut(name: tempCategoryName)
-                tempCategoryName = "" // Reset after addinng
-                dismiss()
+            .onSubmit(handleSubmit)
+        }
+    }
+    
+    private var categoryTextField: some View {
+        TextField(textFieldPlaceholder, text: $tempCategoryName)
+            .textFieldStyle(.roundedBorder)
+            .padding(.bottom, 20)
+    }
+    
+    private var textFieldPlaceholder: String {
+        categories.isEmpty ? "Category Name" : "Or, Enter New Category"
+    }
+    
+    private var categoryListOrMessage: some View {
+        Group {
+            if categories.isEmpty {
+                Text("No categories yet!").italic()
+            } else {
+                categoryList
             }
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button("Add") {
-                        insertNewCategoryAndStoreInShortcut(name: tempCategoryName)
-                        tempCategoryName = "" // Reset after addinng
-                    }
+        }
+    }
+    
+    private var categoryList: some View {
+        List(categories, id: \.self) { category in
+            CategoryRow(category: category)
+                .onTapGesture {
+                    setCategoryAndDismiss(category)
                 }
-            }
-        } // NavigationView
-    }
-    
-    var listCategories: some View {
-        
-//        List(categories) { category in
-//            CategoryRow(category: category)
-//                .onTapGesture {
-//                    setCategoryAndDismiss(category: category)
-//                }
-//        }
-        
-        List {
-            ForEach(categories) { category in
-                CategoryRow(category: category)
-                    .onTapGesture {
-                        setCategoryAndDismiss(category: category)
-                    }
-            }
-            .onDelete(perform: deleteCategories)
-        }
-        
-        
-    }
-    
-    func deleteCategories(_ offsets: IndexSet) {
-        for offset in offsets {
-            let category = categories[offset]
-            modelContext.delete(category)
         }
     }
     
-    func insertNewCategoryAndStoreInShortcut(name: String) {
-        guard name.isNotEmpty else { return }
-        
-        let newCategory = Category(name: name)
-        modelContext.insert(newCategory)
-        newCategory.shortcuts.append(shortcut)
-        shortcut.category = newCategory
+    private func handleSubmit() {
+        if tempCategoryName.isNotEmpty {
+            insertNewCategoryAndStoreInShortcut()
+        }
         dismiss()
     }
     
-    func setCategoryAndDismiss(category: Category) {
+    func deleteCategories(_ offsets: IndexSet) {
+        offsets.forEach { modelContext.delete(categories[$0]) }
+    }
+    
+    func insertNewCategoryAndStoreInShortcut() {
+        let newCategory = Category(name: tempCategoryName)
+        modelContext.insert(newCategory)
+        newCategory.shortcuts.append(shortcut)
+        shortcut.category = newCategory
+    }
+    
+    func setCategoryAndDismiss(_ category: Category) {
         shortcut.category = category
         dismiss()
     }
